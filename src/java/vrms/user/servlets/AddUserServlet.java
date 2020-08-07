ad0f0d2d44d968f8f2cf6c90fc99c89a980cba0f
uldindrajith <uldindrajith@students.nsbm.lk>
2020-08-07 09:01:20 +0530

    User Adding Servlet is Now Functional

--- src/java/vrms/user/servlets/AddUserServlet.java
+++ src/java/vrms/user/servlets/AddUserServlet.java
@@ -2,6 +2,7 @@ package vrms.user.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -20,7 +21,7 @@ public class AddUserServlet extends HttpServlet {
         String gender = request.getParameter("gender");
         String phone_no = request.getParameter("phone_no");
         String email = request.getParameter("email");
-        String user_name = request.getParameter("user_role");
+        String user_name = request.getParameter("user_name");
         String user_role = request.getParameter("user_role");
         String password1 = request.getParameter("password1");
         String password2 = request.getParameter("password2");
@@ -38,21 +39,24 @@ public class AddUserServlet extends HttpServlet {
         user.setPassword2(password2);
 
         UserDAO dao = new UserDAO();
-        String result = dao.insert(user);
-        if ((result != null) && (password1.equals(password2))) {
+        
+        String url = request.getServletPath();
+
+        if ((password1.equals(password2)) && (dao.insert(user) != null)) {
             PrintWriter out = response.getWriter();
             response.setContentType("text/html");
             out.println("<script type=\"text/javascript\">");
-            out.println("window.alert('" + result + " Added!');");
-            out.println("location='web_content\\users\\new_user.jsp';");
+            out.println("window.alert('User Added Successfully!');");
+            out.println("location='web_content/users/new_user.jsp';");
             out.println("</script>");
         } else {
             PrintWriter out = response.getWriter();
             response.setContentType("text/html");
             out.println("<script type=\"text/javascript\">");
-            out.println("window.alert('User could not be added! \n Please Try Again!');");
-            out.println("location='web_content\\users\\new_user.jsp';");
+            out.println("window.alert('Something is Wrong! Please Check Again!');");
+            out.println("location='web_content/users/new_user.jsp';");
             out.println("</script>");
+            
         }
 
     }
