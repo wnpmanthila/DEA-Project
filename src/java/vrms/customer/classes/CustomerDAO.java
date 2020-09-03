1b7de7f1864fc0758c99e716abe257425e0a5e8c
Kavinda <70326018+jgc-kavinda@users.noreply.github.com>
2020-09-03 19:05:42 +0530

    Customer ADD ok

--- src/java/vrms/customer/classes/CustomerDAO.java
+++ src/java/vrms/customer/classes/CustomerDAO.java
@@ -5,10 +5,42 @@
  */
 package vrms.customer.classes;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
- * @author uldin
+ * @author Kavinda
  */
 public class CustomerDAO {
-    
+
+    public int insert(CustomerBean customer) {
+        int result = 0;
+        try {
+            String url = "jdbc:mysql://localhost:3306/vrms_db";
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(url, "root", "");
+            PreparedStatement ps = con.prepareStatement("INSERT INTO customer VALUES (?,?,?,?,?,?,?,?,?)");
+            ps.setString(1, customer.getFirst_name());
+            ps.setString(2, customer.getLast_name());
+            ps.setString(3, customer.getGender());
+            ps.setString(4, customer.getPhone_no());
+            ps.setString(5, customer.getEmail());
+            ps.setString(6, customer.getNic());
+            ps.setString(7, customer.getDl_no());
+            ps.setString(8, customer.getDob());
+            ps.setString(9, customer.getAddress());
+            result = ps.executeUpdate();
+            ps.close();
+            con.close();
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(CustomerDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
+    }
+
 }
